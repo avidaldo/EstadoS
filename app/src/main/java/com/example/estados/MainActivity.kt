c63338877d1fa0a1e91ccd1b80b353ed6388d70a
avidaldo <avd.formacion@gmail.com>
2022-11-17 19:33:45 +0100

    no funciona con StringBuilder

---
+++ app/src/main/java/com/example/estados/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.estados
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.estados.ui.theme.EstadoSTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            EstadoSTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppContent()
+
+
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppContent() {
+    var linea by remember { mutableStateOf(StringBuilder()) }
+
+    Box(Modifier.padding(30.dp)) {
+        Text(text = linea.toString(), Modifier.align(Alignment.TopCenter))
+        Button(
+            onClick = { linea = linea.append("X") },
+            Modifier.align(Alignment.CenterStart)
+        ) {
+            Text(text = "X")
+        }
+        Button(
+            onClick = { linea = linea.append("O") },
+            Modifier.align(Alignment.CenterEnd)
+        ) {
+
+        }
+    }
+}
+
